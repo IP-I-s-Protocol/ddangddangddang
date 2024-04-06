4acabd6cc370ada9b802452e784ec59f6ac1b8d8
kimpangya <nam74415133@gmail.com>
2024-04-06 22:27:56 +0900

    ♻️ feat(town) : town 레포지토리 합침

--- src/main/java/com/ip/ddangddangddang/domain/town/repository/TownRepository.java
+++ src/main/java/com/ip/ddangddangddang/domain/town/repository/TownRepository.java
@@ -1,11 +1,10 @@
 package com.ip.ddangddangddang.domain.town.repository;
 
 import com.ip.ddangddangddang.domain.town.entity.Town;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
-public interface TownRepository {
-
-    void save(Town town);
-
-    Town findById(Long townId);
+@Repository
+public interface TownRepository extends JpaRepository<Town, Long> {
 
 }
--- src/main/java/com/ip/ddangddangddang/domain/town/townList/TownListRepository.java
+++ src/main/java/com/ip/ddangddangddang/domain/town/townList/TownListRepository.java
@@ -1,21 +1,9 @@
 package com.ip.ddangddangddang.domain.town.townList;
 
-import java.util.List;
-import lombok.RequiredArgsConstructor;
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-@RequiredArgsConstructor
 @Repository
-public class TownListRepository {
-
-    private final TownListJpaRepository townListJpaRepository;
-
-    public void saveAll(List<TownList> townList) {
-        townListJpaRepository.saveAll(townList);
-    }
-
-    public List<TownList> findAll() {
-        return townListJpaRepository.findAll().stream().toList();
-    }
+public interface TownListRepository extends JpaRepository<TownList, Long> {
 
 }
